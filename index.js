7c944802e98d73750138dabfa1e0f32b62d0f03c
Rodolfo Recordon <rodolforecordon@gmail.com>
2020-07-23 23:43:42 -0300

    getting dollar cotation from Banco Central do Brasil

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const path = require('path')
 const port = 3000
 
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiBCB.getCotacao()
+    res.render('home', { cotacao })
 })
 
 app.get('/cotation', (req, res) => {
